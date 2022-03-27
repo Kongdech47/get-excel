a682a09e563752df139ab98c4442b367aa9e4b96
Kongdech47 <45354109+Kongdech47@users.noreply.github.com>
2022-03-27 15:44:05 +0700

    first commit

---
+++ get_excel.js
@@ -0,0 +1,69 @@
+var startRow = 7;
+var excelData = [];
+var mapCell = [
+    {
+        'cell': 'A',
+        'key': 'name',
+    },
+    {
+        'cell': 'B',
+        'key': 'num',
+    } 
+];
+
+$("#fileUploader").change(function(evt) {
+    var selectedFile = evt.target.files[0];
+
+    if (!selectedFile == false) {
+        var XLreader = new FileReader();
+        var XL_row_object = {};
+        var workbook = null;
+        var XLdata = null;
+
+        XLreader.onload = function(event) {
+            excelData = [];
+            XLdata = event.target.result;
+            workbook = XLSX.read(XLdata, {
+                type: 'binary'
+            });
+
+            workbook.SheetNames.forEach(function(sheetName) {
+                XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName], {
+                    header: "A",
+                    range: startRow-1,
+                    defval: "",
+                });
+            });
+
+            if(mapCell.length > 0){
+                var mapCellKey = [];
+                mapCell.forEach(function(v) {
+                    mapCellKey[v.cell] = v.key;
+                });
+                
+                XL_row_object = XL_row_object.map(function(val, index) {
+                    var newArray = [];
+                    for (var key in val) {
+                        if(mapCellKey[key]){
+                            newArray[mapCellKey[key]] = val[key];
+                        }else{
+                            newArray[key] = val[key];
+                        }
+                    }
+                    return newArray;
+                });
+            }
+
+            excelData = XL_row_object;
+            console.log(excelData);
+        };
+
+        XLreader.onerror = function(event) {
+            console.error("File could not be read! Code " + event.target.error.code);
+        };
+
+        XLreader.readAsBinaryString(selectedFile);
+    } else {
+        console.log('Fail');
+    }
+});
